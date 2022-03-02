2d7ed4a9faba43cc54098e24614a98bac82ebc66
KwonSongji <mariecamillecalone@gmail.com>
2022-03-02 17:33:22 +0100

    home component

---
+++ ecommerce nft/src/Components/Home/HomeLeft/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const HomeLeft = () => {
+  return (
+    <div className="Home_Left">
+        <h1 className="Home_Left_title"> Best <span> NFT </span> collection for your Future</h1>
+        <div className="Home_Left_btn">
+          <button class="btn btn--full"> </button>
+        </div>
+        <div className="Home_Left_btn">
+          <button class="btn btn--border">
+          </button>
+        </div>
+        <ul>
+          <li>
+            <strong>62 500 </strong>
+            <span>Collection</span>
+          </li>
+          <li>
+            <strong>14 000</strong>
+            <span>Auction</span>
+          </li>
+          <li>
+            <strong>5 200</strong>
+            <span>Artiste</span>
+          </li>
+        </ul>
+      </div>
+  )
+}
+
+export default HomeLeft
